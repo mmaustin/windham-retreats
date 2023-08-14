bb09da42cb9a9dfbc8c8a43ca35b00488debe8c5
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-08-14 10:53:35 -0400

    set up test express server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('server derver');
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Here we go again!');
+})
+
+app.listen(5001, () => {
+  console.log('server running . . .');
+});
