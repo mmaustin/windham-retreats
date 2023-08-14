a2da1298fd9f2604f9a9c5d093a06e9d8c11996a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-08-14 15:16:00 -0400

    added the morgan library to serrver js

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import express from 'express';
 const app = express();
+import morgan from 'morgan';
+
+app.use(morgan('dev'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Here we go again!');
 })
 
+app.post('/', (req, res) => {
+  res.json({message: 'data is data.', data: req.body});
+})
+
 app.listen(5001, () => {
   console.log('server running . . .');
 });
+
+//use the Ed Roh react e-commerce youtube video to structure the frontend
