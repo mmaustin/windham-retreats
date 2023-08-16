cc9068c071d12ca7119909cc281887b962257296
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-08-16 14:34:32 -0400

    added temporary not-found and error handler middleware to server file

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-import express from 'express';
+const express = require('express');
 const app = express();
-import morgan from 'morgan';
+const morgan = require('morgan');
+require('dotenv').config();
 
-app.use(morgan('dev'));
+
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -13,8 +18,19 @@ app.post('/', (req, res) => {
   res.json({message: 'data is data.', data: req.body});
 })
 
-app.listen(5001, () => {
-  console.log('server running . . .');
+app.use('*', (req, res) => {
+  res.status(404).json({msg: "route not found"});
+})
+
+app.use((err, req, res, next)=>{
+  console.log(err);
+  res.status(500).json({msg: 'something went wrong'})
+})
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+  console.log(`server running ${port} ...`);
 });
 
 //use the Ed Roh react e-commerce youtube video to structure the frontend
