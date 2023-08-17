56b6ca33fb5bb74631167a67fd8d7724c25f2249
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-08-17 15:26:45 -0400

    get message controller minus custom status code

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -5,11 +5,12 @@ const createMessage = async(req, res) =>{
   const { messenger, content } = req.body;
 
   if(!messenger || !content){
-    throw new Error('Please provide all values.');
+    res.status(400).json({msg: 'Please provide all values.'});
+    return;
   }
-  
+
   const message = await Message.create(req.body);
-  res.status(200).json({message});
+  res.status(201).json({message});
 };
 
 const getMessages = async(req, res) =>{
@@ -17,7 +18,16 @@ const getMessages = async(req, res) =>{
 };
 
 const getMessage = async(req, res) =>{
-  res.json({message: 'we have the message.'})
+  const {id: messageId} = req.params;
+
+  const message = await Message.findOne({_id: messageId});
+
+  if (!message) {
+    res.status(400).json({msg: 'Message not found.'});
+    return;
+  }
+
+  res.status(200).json({ message });
 };
 
 
