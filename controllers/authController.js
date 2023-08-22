168a0d31cee5e249db3d0f64ba0599f0721137e3
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-08-22 12:44:44 -0400

    created auth routes

---
+++ controllers/authController.js
@@ -0,0 +1,14 @@
+
+
+const register = async (req, res) => {
+  res.send('register');
+};
+
+const login = async (req, res) => {
+  res.send('register');
+};
+
+module.exports = {
+  register,
+  login
+};
