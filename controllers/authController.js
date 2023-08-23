972d79f99edb116e0c13a15440ce06ddac426503
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-08-23 12:01:57 -0400

    set up a utils file for to hash a password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,16 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
 const bcrypt = require('bcryptjs');
+const hashPassword = require("../utils/passwordUtils");
+
 
 
 const register = async (req, res) => {
 
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-  req.body.password = hashedPassword;
-  
+  req.body.password = await hashPassword(req.body.password);
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({user});
+  res.status(StatusCodes.CREATED).json({msg: 'user created'});
 };
 
 const login = async (req, res) => {
