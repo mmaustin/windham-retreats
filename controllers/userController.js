f765333855d89ed4c71a439678d75405808121da
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-08-26 12:04:58 -0400

    restricted admin routes to admin user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
 const Message = require("../models/Message");
+const { UnauthorizedError } = require("../errors");
 
 const getCurrentUser = async (req, res) => {
   const user = await User.findOne({_id: req.user.userId});
@@ -9,10 +10,17 @@ const getCurrentUser = async (req, res) => {
 };
 
 const getApplicationStats = async (req, res) => {
-  res.status(StatusCodes.OK).json({msg: "application stats"})
+  if(req.user.role !== 'admin'){
+    throw new UnauthorizedError('not authorized to view these stats')
+  }
+
+  const users = await User.find({});
+  const messages = await Message.find({});
+  res.status(StatusCodes.OK).json({users: users, msg: messages})
 };
 
 const updateUser = async (req, res) => {
+  
   const obj = {...req.body};
   delete obj.password;
   const updatedUser = await User.findByIdAndUpdate(req.user.userId, obj);
