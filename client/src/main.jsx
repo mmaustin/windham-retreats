f73c65c5d9528bf7158ca049ec0f189fcaae600c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-09-04 11:49:47 -0400

    provided theme to the entire app with theme provider in main jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,36 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import authReducer from './state/index.js';
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
-import {
-  persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST,
-  PURGE, REGISTER
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { PersistGate } from 'redux-persist/integration/react';
 
-const persistConfig = {key: "root", storage, version: 1};
-const persistedReducer = persistReducer(persistConfig, authReducer);
-const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => 
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-      },
-    }),
-});
+// import authReducer from './state/index.js';
+// import { configureStore } from '@reduxjs/toolkit';
+// import { Provider } from 'react-redux';
+// import {
+//   persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST,
+//   PURGE, REGISTER
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import { PersistGate } from 'redux-persist/integration/react';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { theme } from './theme.js';
+
+// const persistConfig = {key: "root", storage, version: 1};
+// const persistedReducer = persistReducer(persistConfig, authReducer);
+// const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) => 
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+//       },
+//     }),
+// });
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <ThemeProvider theme={theme} >
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+    {/* <Provider store={store}>
       <PersistGate loading={null} persistor={persistStore(store)}>
       <App />
       </PersistGate>   
-    </Provider>
+    </Provider> */}
   </React.StrictMode>,
 )
