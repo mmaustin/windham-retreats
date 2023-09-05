9e73f7f982db6eae6c56e677163be571fcf3149c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-09-05 08:16:46 -0400

    crated component files and folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
-import Home from './scenes/home/Home';
+import Home from './pages/home/Home';
 
 const ScrollToTop = () => {
   const {pathname} = useLocation();
