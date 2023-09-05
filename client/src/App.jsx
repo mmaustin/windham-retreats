5a23a63dfa6b90d871f5cae6a960384a0ef628ed
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-09-05 09:29:37 -0400

    created navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
 import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
 import Home from './pages/home/Home';
+import ItemDetails from "./pages/itemDetails/ItemDetails";
+import Checkout from "./pages/checkout/Checkout";
+import Confirmation from "./pages/checkout/Confirmation";
+import Navbar from "./pages/global/Navbar";
+
 
 const ScrollToTop = () => {
   const {pathname} = useLocation();
@@ -16,9 +21,13 @@ const App = () => {
   return (
     <div className="app">
       <BrowserRouter>
+        <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/item/:itemId' element={<ItemDetails />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/checkout/success' element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css';
 
 // import authReducer from './state/index.js';
 // import { configureStore } from '@reduxjs/toolkit';
-// import { Provider } from 'react-redux';
+//import { Provider } from 'react-redux';
 // import {
 //   persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST,
 //   PURGE, REGISTER
@@ -32,10 +32,12 @@ import { theme } from './theme.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <Provider> */}
     <ThemeProvider theme={theme} >
       <CssBaseline />
       <App />
     </ThemeProvider>
+    {/* </Provider> */}
     {/* <Provider store={store}>
       <PersistGate loading={null} persistor={persistStore(store)}>
       <App />
