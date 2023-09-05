685e45e7986fdd6e14f610f89bba1e4d62351d4c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-09-05 10:03:47 -0400

    added more actions to authslice reducer

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,7 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  loggedIn: false,
+  //loggedIn: false,
+  isCartOpen: false,
+  cart: [],
+  items: [],
 };
 
 export const authSlice = createSlice({
@@ -10,9 +13,37 @@ export const authSlice = createSlice({
   reducers: {
     setLogin: (state, action) => {
       state.loggedIn = action.payload.loggedIn;
-    },    
+    },
+    setItems: (state, action) => {
+      state.items = action.payload;
+    },
+    addToCart: (state, action) => {
+      state.cart = [...state.cart, action.payload.item];
+    },
+    removeFromCart: (state, action) => {
+      state.cart = state.cart.filter(item => item.id !== action.payload.id);
+    },
+    increaseCount: (state, action) => {
+      state.cart = state.cart.map(item => {
+        if(item.id === action.payload.id){
+          item.count++;
+        }
+        return item;
+      })
+    },
+    decreaseCount: (state, action) => {
+      state.cart = state.cart.map(item => {
+        if(item.id === action.payload.id  && item.count > 1){
+          item.count--;
+        }
+        return item;
+      })
+    },
+    setIsCartOpen: state => {
+      state.isCartOpen = !state.isCartOpen;
+    }
   },
 });
 
-export const { setLogin } = authSlice.actions;
+export const { setLogin, setItems, addToCart, removeFromCart, increaseCount, decreaseCount, setIsCartOpen } = authSlice.actions;
 export default authSlice.reducer;
