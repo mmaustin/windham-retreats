481e4ecfbf0a8feb0e52d17f10c3d601e5c779e2
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-09-05 08:03:57 -0400

    created scroll to top of page funciton in app jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,39 @@
+import { useEffect } from "react";
+import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
+import Home from './scenes/home/Home';
+
+const ScrollToTop = () => {
+  const {pathname} = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0,0);
+  },[pathname])
+
+  return null;
+}
+
+const App = () => {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+export default App
 //import { useState, useEffect } from 'react'
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
-import Register from './pages/Register';
-import CheckOut from './pages/CheckOut';
-import { useSelector } from 'react-redux';
+// import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
+// import Register from './pages/Register';
+// import CheckOut from './pages/CheckOut';
+// import { useSelector } from 'react-redux';
 
 
-function App() {
+
+// function App() {
   //const [count, setCount] = useState(0);
 
   // const getObject = async() => {
@@ -19,21 +47,26 @@ function App() {
   //   getObject();
   // }, []);
 
-  const isLoggedIn = useSelector(state => state.loggedIn);
+  //const isLoggedIn = useSelector(state => state.loggedIn);
 
-  return (
-    <div className='app'>   
-      <BrowserRouter>
-        <Routes>
-          <Route path='/register' element={<Register />} />
-          <Route path='/checkout' element={isLoggedIn ? < CheckOut /> : <Navigate to="/register"/>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-    // <figure >
-    //   <img src="./src/assets/yoga-women.jpg" alt="yoga women" />
-    // </figure>
-  )
-}
+//   return (
+//     <div className='app'>
+//       <h1>hello</h1>
+//       <h2>hello</h2>
+//       <h3>hello</h3>
+//       <h4>hello</h4>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path='/register' element={<Register />} />
+//           <Route path='/checkout' element={isLoggedIn ? < CheckOut /> : <Navigate to="/register"/>} />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   )
+// }
 
-export default App;
+// export default App;
+
+// <figure >
+//   <img src="./src/assets/yoga-women.jpg" alt="yoga women" />
+// </figure>
