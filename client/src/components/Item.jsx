ce4c3fc08372ee6013b79cfb1378a29362929a8e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-09-06 12:06:11 -0400

    completed jsx for item component

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -20,8 +20,63 @@ const Item = ({item, width}) => {
   const { unitAmount, name, picturePath } = item;
 
   return (
-    <Box width={width}>
+    <Box width={width} >
+      <Box
+        position='relative'
+        onMouseOver={()=> setIsHovered(true)}
+        onMouseOut={()=> setIsHovered(false)}
+      >
+        <img
+          alt={item.name}
+          width='300px'
+          height='400px'
+          src={`../src/assets${picturePath}`}
+          onClick={()=> navigate(`/item/${item._id}`)}
+          style={{cursor: 'pointer'}}
+        />
+        <Box
+          display={isHovered ? 'block' : 'none'}
+          position='absolute'
+          bottom='10px'
+          left='0'
+          width='100%'
+          padding='0 5%'
+        >
+          <Box display='flex' justifyContent='space-between'>
+            <Box
+              display='flex'
+              alignItems='center'
+              bgcolor={shades.neutral[100]}
+              borderRadius='3px'
+            >
+              <IconButton
+                onClick={()=> setCount(Math.max(count -1, 1))}
+              >
+                <Remove />
+              </IconButton>
+              <Typography color={shades.primary[300]}>{count}</Typography>
+              <IconButton
+                onClick={()=> setCount(count + 1)}
+              >
+                <Add />
+              </IconButton>              
+            </Box>
 
+            <Button
+              onClick={()=> {dispatch(addToCart({item: {...item, count}}))}}
+              sx={{backgroundColor: shades.primary[300], color: 'white'}}
+            >
+              Add To Cart
+            </Button>
+
+          </Box>
+        </Box>
+      </Box>
+
+      <Box mt='3px'>
+        <Typography>{name}</Typography>
+        <Typography fontWeight='bold'>${unitAmount}</Typography>
+      </Box>
     </Box>
   )
 }
