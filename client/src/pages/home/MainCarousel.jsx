ca89239dca86877b8075934f489f9bf546e57ec7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-09-07 08:40:05 -0400

    applied mui styles to carousel before and next buttons

--- client/src/pages/home/MainCarousel.jsx
+++ client/src/pages/home/MainCarousel.jsx
@@ -3,7 +3,18 @@ import {Carousel} from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import {NavigateBefore, NavigateNext} from '@mui/icons-material';
 import {shades} from '../../theme';
-import require from 'require-context/';
+import require from 'require-context';
+import retreatOne from '../../assets/retreat-1.jpg';
+import retreatTwo from '../../assets/retreat-2.jpg';
+import retreatThree from '../../assets/retreat-3.jpg';
+import yogaWomen from '../../assets/yoga-women.jpg';
+
+const carouselPictures = {
+  retreat1: retreatOne,
+  retreat2: retreatTwo,
+  retreat3: retreatThree,
+  yogaW: yogaWomen
+};
 
 const importAll = (r) => 
   r.keys().reduce((acc, item) => {
@@ -17,8 +28,48 @@ const importAll = (r) =>
 
 
 const MainCarousel = () => {
+
+  const isNonMobile = useMediaQuery("(min-width: 600px");
+
   return (
-    <div>MainCarousel</div>
+    <Carousel
+      infiniteLoop={true}
+      showThumbs={false}
+      showIndicators={false}
+      showStatus={false}
+      renderArrowPrev={(onClickHandler, hasPrev, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: '0',
+            color: 'white',
+            padding: '5px',
+            zIndex: '10'
+          }}
+        >
+          <NavigateBefore sx={{fontSize: 40}} />
+        </IconButton>
+      )}
+      renderArrowNext={(onClickHandler, hasNext, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            right: '0',
+            color: 'white',
+            padding: '5px',
+            zIndex: '10'
+          }}
+        >
+          <NavigateNext sx={{fontSize: 40}} />
+        </IconButton>
+      )}
+    >
+
+    </Carousel>
   )
 }
 export default MainCarousel;
