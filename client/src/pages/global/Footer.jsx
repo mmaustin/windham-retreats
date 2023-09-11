ea103ddc79c2511a713f5e4c28f1e328fc26448b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-09-11 09:06:54 -0400

    created a simple footer component

--- client/src/pages/global/Footer.jsx
+++ client/src/pages/global/Footer.jsx
@@ -8,7 +8,43 @@ const Footer = () => {
 
   return (
     <Box mt='70px' p='40px 0' bgcolor={info.light}>
-      Footer
+      <Box
+        width='80%'
+        margin='auto'
+        display='flex'
+        justifyContent='space-around'
+        flexWrap='wrap'
+        rowGap='30px'
+        columnGap='clamp(20px, 30px, 40px)'
+      >
+        <Box width='clamp(20%, 30%, 40%)'>
+          <Typography variant='h4' fontWeight='bold' mb='30px' color={shades.secondary[500]}>
+            Windom Yoga
+          </Typography>
+          <div>
+            Let us help you rediscover yourself and unlock your true potential.
+          </div>
+        </Box>
+
+        <Box width='clamp(20%, 30%, 40%)'>
+          <Typography variant='h4' fontWeight='bold' mb='30px' color={shades.secondary[500]}>
+            Curator: Erica Ahse, PhD
+          </Typography>
+        </Box>
+
+        <Box width='clamp(20%, 30%, 40%)'>
+          <Typography variant='h4' fontWeight='bold' mb='30px' color={shades.secondary[500]}>
+            Contact Us:
+          </Typography>
+          <Typography variant='h4' fontWeight='bold' mb='30px' color={shades.secondary[500]}>
+            555-555-5555
+          </Typography>
+          <Typography variant='h4' fontWeight='bold' mb='30px' color={shades.secondary[500]}>
+            windhamyoga@gmail.com
+          </Typography>
+        </Box>
+
+      </Box>
     </Box>
   )
 }
