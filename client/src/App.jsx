2c136b501eb6aad38b1e88b422f00ea9139361f5
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-09-15 12:05:50 -0400

    created payment and completion stripe test components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 // import { useEffect } from "react";
-// import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
+import Payment from './stripePractice/Payment';
+import Completion from './stripePractice/Completion';
 // import Home from './pages/home/Home';
 // import ItemDetails from "./pages/itemDetails/ItemDetails";
 // import Checkout from "./pages/checkout/Checkout";
@@ -50,93 +52,36 @@
 
 
 
-// function App() {
-  //const [count, setCount] = useState(0);
+function App() {
+  // const [count, setCount] = useState(0);
 
   // const getObject = async() => {
-    //const response = await axios.get("/api/v1/messages");
+  //   const response = await axios.get("/api/v1/messages");
 
-    //const { data } = await response.json();
-    //console.log(response.data);
-  //}
+  //   const { data } = await response.json();
+  //   console.log(response.data);
+  // }
 
   // useEffect(() => {
   //   getObject();
   // }, []);
 
-  //const isLoggedIn = useSelector(state => state.loggedIn);
+  // const isLoggedIn = useSelector(state => state.loggedIn);
 
-//   return (
-//     <div className='app'>
-//       <h1>hello</h1>
-//       <h2>hello</h2>
-//       <h3>hello</h3>
-//       <h4>hello</h4>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path='/register' element={<Register />} />
-//           <Route path='/checkout' element={isLoggedIn ? < CheckOut /> : <Navigate to="/register"/>} />
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
-//   )
-// }
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Payment />} />
+          <Route path='/completion' element={<Completion />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
 
-// export default App;
+export default App;
 
 // <figure >
 //   <img src="./src/assets/yoga-women.jpg" alt="yoga women" />
 // </figure>
-import  { useState, useEffect } from "react";
-//import "./App.css";
-
-const ProductDisplay = () => (
-  <section>
-    <div className="product">
-      <img
-        src="https://i.imgur.com/EHyR2nP.png"
-        alt="The cover of Stubborn Attachments"
-      />
-      <div className="description">
-      <h3>Stubborn Attachments</h3>
-      <h5>$20.00</h5>
-      </div>
-    </div>
-    <form action="/create-checkout-session" method="POST">
-      <button type="submit">
-        Checkout
-      </button>
-    </form>
-  </section>
-);
-
-const Message = ({ message }) => (
-  <section>
-    <p>{message}</p>
-  </section>
-);
-
-export default function App() {
-  const [message, setMessage] = useState("");
-  console.log(message);
-  useEffect(() => {
-    // Check to see if this is a redirect back from Checkout
-    const query = new URLSearchParams(window.location.search);
-
-    if (query.get("success")) {
-      setMessage("Order placed! You will receive an email confirmation.");
-    }
-
-    if (query.get("canceled")) {
-      setMessage(
-        "Order canceled -- continue to shop around and checkout when you're ready."
-      );
-    }
-  }, []);
-
-  return message ? (
-    <Message message={message} />
-  ) : (
-    <ProductDisplay />
-  );
-}
---
+++ client/src/stripePractice/Completion.jsx
@@ -0,0 +1,8 @@
+
+
+const Completion = () => {
+  return (
+    <div>Completion</div>
+  )
+}
+export default Completion
