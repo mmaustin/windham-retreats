10de94b2df56226fe9153a9db9afdb6ff424502a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-09-19 11:30:42 -0400

    added item picture to payment display

--- client/src/stripeFiles/Payment.jsx
+++ client/src/stripeFiles/Payment.jsx
@@ -3,7 +3,16 @@ import {loadStripe} from '@stripe/stripe-js';
 import CheckoutForm from "./CheckoutForm";
 import {Elements} from '@stripe/react-stripe-js';
 import { useSelector } from "react-redux";
+import { Box, Divider, IconButton, Typography} from '@mui/material';
+import { Close, Add, Remove } from "@mui/icons-material";
+import { shades } from "../theme";
+import styled from '@emotion/styled';
 
+const FlexBox = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
 
 const Payment = () => {
 
@@ -41,6 +50,56 @@ const Payment = () => {
   return (
     <div style={{marginTop: '60px'}}>
       <h1>Add Payment Information</h1>
+      <Box>
+        {cart.map(item => (
+          <Box key={`${item.name}-${item._id}`}>
+            <FlexBox p='15px 0' >
+              <Box flex='1 1 40%'>
+                <img
+                  src={`../../src/assets/${item.picturePath}`}
+                  alt={item?.name}
+                  height='164px'
+                  width='123px'
+                />
+              </Box>
+              <Box flex='1 1 60%'>
+                <FlexBox mb='5px'>
+                  <Typography fontWeight='bold'>
+                    {item.name}
+                  </Typography>
+                  {/* <IconButton onClick={()=> dispatch(removeFromCart({id: item._id}))} >
+                    <Close />
+                  </IconButton> */}
+                </FlexBox>
+                <Typography >{item.description}</Typography>
+                <FlexBox m='15px 0'>
+                  <Box 
+                    display='flex'
+                    alignItems='center'
+                    border={`1.5px solid ${shades.neutral[500]}`}
+                  >
+                    <IconButton
+                      // onClick={()=> dispatch(decreaseCount({id: item._id}))}
+                    >
+                      <Remove />
+                    </IconButton>
+                    <Typography>{item.count}</Typography>
+                    <IconButton
+                      // onClick={()=> dispatch(increaseCount({id: item._id}))}
+                    >
+                      <Add />
+                    </IconButton>                        
+                  </Box>
+                  <Typography fontWeight='bold'>
+                    {item.displayAmount}
+                  </Typography>
+                </FlexBox>
+              </Box>
+            </FlexBox>
+            <Divider />
+          </Box>
+        ))}
+      </Box>
       <p>${totalPrice}</p>
       {clientSecret && stripePromise && (
         <Elements stripe={stripePromise} options={{ clientSecret}}>
