5b4ad4b9c68580b40f3662901a9621f519ccdf81
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-09-26 10:27:38 -0400

    pasted previous project's code in customer info component to test mui dialog props

--- client/src/components/CustomerInfo.jsx
+++ client/src/components/CustomerInfo.jsx
@@ -5,6 +5,8 @@ import { useSelector } from 'react-redux';
 const CustomerInfo = () => {
 
   const [open, setOpen] = useState(false);
+  const customer = useSelector(state => state.customer);
+  console.log(customer);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -15,7 +17,33 @@ const CustomerInfo = () => {
   };  
 
   return (
-    <div>CustomerInfo</div>
+    <div>
+      <Button variant="contained" onClick={handleClickOpen}>
+        How It Works
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Like Life, Random History Ain't Always Fair!"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            1) Clicking a cell generates a random number from one to six.
+            2) If the random numbers matches the number of the clicked cell, you get awarded points
+               that equal the number of the cell; the cell turns green.
+            3) If the numbers don't match, your score is reduced by the number on the cell, and you are taken
+               to the quiz area. 
+            4) Answer correctly and get awarded points equaling the number on the cell plus an additional point.
+               There is no penalty for getting the answer wrong.
+            5) If the question is answered correctly, the cell turns beige; otherwise, the cell turns black.
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    </div>
   )
 }
 export default CustomerInfo
