264bd6e5621445b37eae25d962791441a93942c6
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-10-02 12:06:33 -0400

    set up regex validation for zip code and phone number on the user model

--- models/User.js
+++ models/User.js
@@ -4,19 +4,32 @@ const UserSchema = new mongoose.Schema({
   name: String,
   lastName: String,
   email: String,
-  phoneNumber: String,
-  zipCode: String,
+  phoneNumber: {
+    type: String,
+    required: [true, 'Please include your phone number.'],
+    match: [
+      /^\d{3}-\d{3}-\d{4}$/,
+      'Please provide a valid phone number.'
+    ]
+  },
+  zipCode: {
+    type: String,
+    match: [
+      /^\d{5}/,
+      'Please provide a valid zip code.'
+    ]
+  },
   // password: String,
   role: {
     type: String,
     enum: ['user', 'admin'],
     default: 'user'
   },
-  },
-  {timestamps: true}
+},
+  { timestamps: true }
 );
 
-UserSchema.methods.toJSON = function(){
+UserSchema.methods.toJSON = function () {
   let obj = this.toObject();
   delete obj.password;
   return obj;
